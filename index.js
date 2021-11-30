a978e8edaf3807cf8431ae3bf3edf1ee1c3e5c0e
Logan <loganfranklinsmith@hotmail.com>
2021-11-30 01:27:17 -0500

    commit tree 33466be542bcc3e9bbe51393442c6554ecc585f4

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(driverList, name) {
+    const result = driverList.filter(driver => driver.toLowerCase() === name.toLowerCase());
+    return result;
+};
+
+function fuzzyMatch(driverList, letter){
+    const foundInitital = driverList.filter(driver => driver.toLowerCase().charAt(0) === letter.toLowerCase().charAt(0));
+    return foundInitital;
+};
+
+function matchName(driverList, driverName) {
+    const result = driverList.filter(driver => driver.name.toLowerCase() === driverName.toLowerCase());
+    return result;
+};
